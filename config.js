99d2791350f0f00ed483852a4f09a41c9e224147
Q <53419500+icoser@users.noreply.github.com>
2024-03-24 11:51:39 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const token = '';
-const api = {
-    info:'https://czgy.xmanju.com:5001/api/auth/tokenInfo',
-    add2:'https://czgy.xmanju.com:5001/api/sys/operationLog/renter/add',
-    ada1:'https://czgy.xmanju.com:5001/api/rntWaitRoom/add',
-    getroom:'https://czgy.xmanju.com:5001/api/hsProject/listAll',
-    getlist:'https://czgy.xmanju.com:5001/api/hsHouseType/listAll'
-}
-module.exports = {token,api};
